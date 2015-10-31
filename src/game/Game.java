1eb5b3a34f5fcf1c0f3afbb20d93deb0c36d457b
Ivan Yanchev <ivan.yan4ev@gmail.com>
2015-10-31 21:13:11 +0200

    changed background picture and window size

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,9 +2,11 @@ package game;
 
 import display.Display;
 import gfx.ImageLoader;
+import gfx.SpriteSheet;
 
 import java.awt.*;
 import java.awt.image.BufferStrategy;
+import java.util.Random;
 
 public class Game implements Runnable {
     private String title;
@@ -17,6 +19,7 @@ public class Game implements Runnable {
     private Display display;
     private BufferStrategy bs;
     private Graphics graphics;
+    private SpriteSheet spsh;
 
     private Font statsFont = new Font("Monospaced", Font.BOLD | Font.ITALIC, 22);
 
@@ -29,6 +32,7 @@ public class Game implements Runnable {
 
     private void init() {
         this.display = new Display(title, width, height);
+        this.spsh = new SpriteSheet(ImageLoader.loadImage("/images/blocks.png"));
     }
 
     private void tick() {
@@ -45,12 +49,23 @@ public class Game implements Runnable {
 
         this.graphics = this.bs.getDrawGraphics();
 
-        this.graphics.drawImage(ImageLoader.loadImage("/images/background.png"), 0, 0, null);
+        this.graphics.drawImage(ImageLoader.loadImage("/images/background.png"), 0, 20, null);
+
+        // THIS IS SOME RANDOM BLOCKS PRINTING JUST FOR TEST PURPOSE
+        Random rnd = new Random();
+        for (int row = 1; row <= 20; row++) {
+            for (int col = 1; col <= 10; col++) {
+                int color = rnd.nextInt(8);
+                this.graphics.drawImage(this.spsh.crop(color * 24, 0, 24, 25), col * 24, 20 + row * 24, null);
+            }
+        }
 
         this.graphics.setFont(statsFont);
         //this.graphics.drawString("12", 270, 254);
         //this.graphics.drawString("3698", 270, 304);
         //this.graphics.drawString("987", 270, 354);
+
+        // END OF TEST PRINTING
 
         this.bs.show();
         this.graphics.dispose();
--- src/game/Launcher.java
+++ src/game/Launcher.java
@@ -4,7 +4,7 @@ import display.Display;
 
 public class Launcher {
     public static void main(String[] args) {
-        Game game = new Game("Tetris", 456, 528);
+        Game game = new Game("Tetris", 456, 548);
         game.start();
     }
 }
