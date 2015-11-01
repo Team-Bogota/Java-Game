a0983dbac03b51cd858f892d162735082ea92568
Aleksandar Tanev <aleksander.tanev.51@gmail.com>
2015-11-01 03:35:43 +0200

    Made changes to the Game() class by moving the main code of the game to the GameState class. Also added the ability to choose which state to be started. This way we can work on separate states (like the Menu) and choose from the Game() class which one to be started and at what point.

--- src/states/GameState.java
+++ src/states/GameState.java
@@ -5,6 +5,9 @@ import gfx.ImageLoader;
 
 import java.awt.*;
 import java.awt.image.BufferStrategy;
+import java.util.Random;
+
+import gfx.SpriteSheet;
 
 // This is the main state // AleksandarTanev
 public class GameState extends State {
@@ -12,11 +15,15 @@ public class GameState extends State {
     private Display display;
     private BufferStrategy bs;
     private Graphics graphics;
+    private SpriteSheet spsh;
+
     private Font statsFont = new Font("Monospaced", Font.BOLD | Font.ITALIC, 22);
 
 
     public GameState(String title, int width, int height) {
+
         this.display = new Display(title, width, height);
+        this.spsh = new SpriteSheet(ImageLoader.loadImage("/images/blocks.png"));
     }
 
 
@@ -38,10 +45,21 @@ public class GameState extends State {
 
         graphics.drawImage(ImageLoader.loadImage("/images/background.png"), 0, 0, null);
 
-        graphics.setFont(statsFont);
+        // THIS IS SOME RANDOM BLOCKS PRINTING JUST FOR TEST PURPOSE
+        Random rnd = new Random();
+        for (int row = 1; row <= 20; row++) {
+            for (int col = 1; col <= 10; col++) {
+                int color = rnd.nextInt(8);
+                this.graphics.drawImage(this.spsh.crop(color * 24, 0, 24, 25), col * 24, 20 + row * 24, null);
+            }
+        }
+
+        this.graphics.setFont(statsFont);
         //this.graphics.drawString("12", 270, 254);
         //this.graphics.drawString("3698", 270, 304);
         //this.graphics.drawString("987", 270, 354);
+
+        // END OF TEST PRINTING
 
         this.bs.show();
         graphics.dispose();
