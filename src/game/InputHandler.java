4f9bb9958723c988f03468353d694ff1033fdc47
Pavel Ilchev <pavel_ilchev@abv.bg>
2015-11-02 18:24:47 +0200

    second type of process move comand

--- src/game/InputHandler.java
+++ src/game/InputHandler.java
@@ -25,21 +25,6 @@ public class InputHandler implements KeyListener {
     @Override
     //all checks are whit string compare - name of configured keys and name of used keys
     public void keyPressed(KeyEvent e) {
-       /* if (KeyEvent.getKeyText(e.getKeyCode()).equals(Config.left)) {
-            left = true;
-        } else if (KeyEvent.getKeyText(e.getKeyCode()).equals(Config.right)) {
-            right = true;
-        } else if (KeyEvent.getKeyText(e.getKeyCode()).equals(Config.down)) {
-            down = true;
-        } else if (KeyEvent.getKeyText(e.getKeyCode()).equals(Config.rotate)) {
-            rotate = true;
-        } else if (KeyEvent.getKeyText(e.getKeyCode()).equals(Config.pause)) {
-            pause = true;
-        }*/
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
         if (KeyEvent.getKeyText(e.getKeyCode()).equals(Config.left)) {
             left = true;
         } else if (KeyEvent.getKeyText(e.getKeyCode()).equals(Config.right)) {
@@ -50,6 +35,21 @@ public class InputHandler implements KeyListener {
             rotate = true;
         } else if (KeyEvent.getKeyText(e.getKeyCode()).equals(Config.pause)) {
             pause = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (KeyEvent.getKeyText(e.getKeyCode()).equals(Config.left)) {
+            left = false;
+        } else if (KeyEvent.getKeyText(e.getKeyCode()).equals(Config.right)) {
+            right = false;
+        } else if (KeyEvent.getKeyText(e.getKeyCode()).equals(Config.down)) {
+            down = false;
+        } else if (KeyEvent.getKeyText(e.getKeyCode()).equals(Config.rotate)) {
+            rotate = false;
+        } else if (KeyEvent.getKeyText(e.getKeyCode()).equals(Config.pause)) {
+            pause = false;
         }
     }
 }
